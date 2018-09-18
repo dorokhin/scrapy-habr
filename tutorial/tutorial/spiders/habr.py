f0c456530d5ad4b16d7324220d4eeaf7c168e479
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-09-18 21:00:20 +0700

    expand parse method

--- tutorial/tutorial/spiders/habr.py
+++ tutorial/tutorial/spiders/habr.py
@@ -19,4 +19,7 @@ class HabrSpider(scrapy.Spider):
             yield {
                 'title': article.css('a.post__title_link::text').extract_first(),
                 'author': article.css('span.user-info__nickname::text').extract_first(),
+                'rating': article.css('span.voting-wjt__counter::text').extract_first(),
+                'link': article.css('a.post__title_link::attr(href)').extract_first(),
+                'post_type': article.css('span.post__type-label::text').extract(),
             }
