fe69b656f415d15dc6a88bf82f45cbe327026dc7
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-09-18 21:00:37 +0700

    add pipeline

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -1,11 +1,15 @@
-# -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
 
 
-class TutorialPipeline(object):
+class JsonWriterPipeline(object):
+
+    def open_spider(self, spider):
+        self.file = open('items.json', 'w', encoding='utf-8')
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
         return item
